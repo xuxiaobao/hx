2a9e560e3d5234c2adea0499657f0cac997064a3
xuxiaobao <2269649004@qq.com>
2017-05-20 18:52:54 +0800

    上游渠道添加

--- src/main/java/com/miaosu/service/recharge/sup/AippRecharge.java
+++ src/main/java/com/miaosu/service/recharge/sup/AippRecharge.java
@@ -19,12 +19,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 /**
  * Created by Administrator on 2017/5/19.
  */
+@Service
 public class AippRecharge extends AbstractRecharge {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
